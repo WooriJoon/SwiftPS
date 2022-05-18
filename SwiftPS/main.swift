c52c1ab0f6658720609292a99e5dcbfb6c72a7ca
ChickenJoah <kpl88@naver.com>
2022-05-18 14:57:40 +0900

    solve problem

--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -7,3 +7,19 @@
 
 import Foundation
 
+let C = Int(readLine()!)!
+
+for _ in 0..<C {
+    let input = readLine()!.split(separator: " ").map() { Int($0)! }
+    var avg: Double = 0
+    var count: Double = 0
+    
+    for i in 1..<input.count { avg += Double(input[i]) }
+    avg /= Double(input[0])
+    
+    for i in 1..<input.count {
+        if Double(input[i]) > avg { count += 1 }
+    }
+    
+    print("\(count / Double(input[0]) * 100)%")
+}
