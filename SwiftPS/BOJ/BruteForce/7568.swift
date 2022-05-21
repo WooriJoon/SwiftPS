4a734e4bcf9609e4439e72745ba144198cfb215d
ChickenJoah <kpl88@naver.com>
2022-05-22 01:12:47 +0900

    solve problem

--- SwiftPS/BOJ/BruteForce/7568.swift
+++ SwiftPS/BOJ/BruteForce/7568.swift
@@ -6,4 +6,52 @@
 //
 
 // 7568 덩치
-import Foundation
+//import Foundation
+//
+//let N = Int(readLine()!)!
+//var people: [[Int]] = []
+//var rank: [Int] = []
+//
+//for _ in 0..<N {
+//    people.append(readLine()!.split(separator: " ").map() { Int($0)! })
+//}
+//
+//for i in 0..<N {
+//    var currRank: Int = 1
+//
+//    for j in 0..<N {
+//        if people[i][0] < people[j][0] && people[i][1] < people[j][1] { currRank += 1 }
+//    }
+//    rank.append(currRank)
+//}
+//
+//for i in 0..<N {
+//    print(rank[i], terminator: " ")
+//}
+
+// 왜 틀림?
+//let N = Int(readLine()!)!
+//var people: [(Int, Int)] = []
+//var rank: [Int] = []
+//
+//for _ in 0..<N {
+//    let input = readLine()!.split(separator: " ").map() { Int($0)! }
+//    people.append((input[0], input[1]))
+//}
+//
+//print(people)
+//
+//for i in 0..<N {
+//    var currRank: Int = 1
+//
+//    for j in 0..<N {
+//        if people[i].0 < people[j].0 && people[i].1 < people[j].1 {
+//            currRank += 1
+//        }
+//    }
+//    rank.append(currRank)
+//}
+//
+//for i in 0..<N {
+//    print(rank[i], terminator: " ")
+//}
--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -8,7 +8,22 @@
 import Foundation
 
 let N = Int(readLine()!)!
+var people: [[Int]] = []
+var rank: [Int] = []
 
 for _ in 0..<N {
+    people.append(readLine()!.split(separator: " ").map() { Int($0)! })
+}
+
+for i in 0..<N {
+    var currRank: Int = 1
     
+    for j in 0..<N {
+        if people[i][0] < people[j][0] && people[i][1] < people[j][1] { currRank += 1 }
+    }
+    rank.append(currRank)
+}
+
+for i in 0..<N {
+    print(rank[i], terminator: " ")
 }
