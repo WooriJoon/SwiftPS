145cb6e1278b4740e80064186bc90069f7527a6c
ChickenJoah <kpl88@naver.com>
2022-05-25 00:40:48 +0900

    solve problem

---
+++ SwiftPS/Programmers/Level 2/방문 길이.swift
@@ -0,0 +1,8 @@
+//
+//  방문 길이.swift
+//  SwiftPS
+//
+//  Created by Chicken on 2022/05/24.
+//
+
+// 방문 길이
--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -7,4 +7,34 @@
 
 import Foundation
 
+//func solution(_ dirs:String) -> Int {
+//    return 0
+//}
 
+//let dirs: String = "ULURRDLLU"
+//var x: Int = 0
+//var y: Int = 0
+//var beforeCoordinate: (Int, Int) = (0, 0)
+//var afterCoordinate: (Int, Int)
+//var route: [(Int, Int)] = []
+//
+//
+//
+//for ch in dirs {
+//    switch ch {
+//    case "U":
+//        guard y != 5 else { break }
+//
+//    case "D":
+//        guard y != -5 else { break }
+//
+//    case "R":
+//        guard x != 5 else { break }
+//
+//    case "L":
+//        guard x != -5 else { break }
+//
+//    default:
+//        break
+//    }
+//}
