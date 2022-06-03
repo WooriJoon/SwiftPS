e4d1fb1162a0aefea5d5c7d403d1078787a1d074
ChickenJoah <kpl88@naver.com>
2022-06-04 02:20:32 +0900

    solve problem

---
+++ SwiftPS/BOJ/Geometry/3009.swift
@@ -0,0 +1,24 @@
+//
+//  3009.swift
+//  SwiftPS
+//
+//  Created by Chicken on 2022/06/04.
+//
+
+// 3009 네 번째 점
+//import Foundation
+
+//var x: [Int] = []
+//var y: [Int] = []
+//
+//for _ in 0..<3 {
+//    let input = readLine()!.split(separator: " ").map() { Int($0)! }
+//    x.append(input[0])
+//    y.append(input[1])
+//}
+//
+//x.sort()
+//y.sort()
+//
+//if x[0] == x[1] { y[0] == y[1] ? print("\(x[2]) \(y[2])") : print("\(x[2]) \(y[0])") }
+//else { y[0] == y[1] ? print("\(x[0]) \(y[2])") : print("\(x[0]) \(y[0])") }
--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -1,35 +1,79 @@
+////
+////  main.swift
+////  SwiftPS
+////
+////  Created by Chicken on 2022/05/15.
+////
 //
-//  main.swift
-//  SwiftPS
-//
-//  Created by Chicken on 2022/05/15.
-//
-
 import Foundation
 
-let input = readLine()!.split(separator: " ").map() { Int($0)! }
-let N = input[0]
-let K = input[1]
-var circularQueue: [Int] = []
-var index: Int = -1
-var result: [Int] = []
+struct Queue {
+    private var Queue: [Int?] = []
+    var front: Int = -1
+    var rear: Int = -1
 
-for i in 1...N { circularQueue.append(i) }
-
-while circularQueue.count > 0 {
-    for _ in 1...K {
-        if index == circularQueue.count - 1 { index = 0 }
-        else { index += 1 }
+    mutating func push(_ X: Int) {
+        self.front += 1
+        Queue.append(X)
     }
-    
-    result.append(circularQueue.remove(at: index))
-    index -= 1
+
+    mutating func pop() {
+        if Queue.isEmpty { print("-1") }
+        else {
+            self.rear += 1
+            print(Queue[rear]!)
+            Queue[rear] = nil
+        }
+    }
+
+    func size() {
+        print(Queue.count)
+    }
+
+    func empty() {
+        if Queue.isEmpty { print("1") }
+        else { print("0") }
+    }
+
+    func printFront() {
+        if Queue.isEmpty { print("-1") }
+        else { print(Queue.first!!) }
+    }
+
+    func printBack() {
+        if Queue.isEmpty { print("-1") }
+        else { print(Queue.last!!) }
+    }
 }
 
-print("<", terminator: "")
-for i in 0..<result.count {
-    if i != result.count - 1 { print("\(result[i]), ", terminator: "") }
-    else { print("\(result[i])>") }
+let N = Int(readLine()!)!
+var queue = Queue()
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map() { String($0) }
+    let command: String = input[0]
+
+    switch command {
+    case "push":
+        let num: Int = Int(input[1])!
+        queue.push(num)
+
+    case "pop":
+        queue.pop()
+
+    case "size":
+        queue.size()
+
+    case "empty":
+        queue.empty()
+
+    case "front":
+        queue.printFront()
+
+    case "back":
+        queue.printBack()
+
+    default:
+        break
+    }
 }
-
-
