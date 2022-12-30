40b35afcede228b8cb00c8fba32dc4e16ae84684
YunJoon <kpl88@naver.com>
2022-12-30 15:51:40 +0900

    [BOJ] 10815 숫자 카드

--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 let N = Int(readLine()!)!
-let givenArray = readLine()!.split(separator: " ").map() { Int($0)! }
+let havingCards = readLine()!.split(separator: " ").map() { Int($0)! }.sorted()
 let M = Int(readLine()!)!
-let checkedArray = readLine()!.split(separator: " ").map() { Int($0)! }
+let compareCards = readLine()!.split(separator: " ").map() { Int($0)! }
 
 func binarySearch(array: [Int], target: Int) -> Bool {
     var min = 0
     var max = array.count - 1
-    
+
     while (min <= max) {
         let mid = (min + max) / 2
-        
+
         if array[mid] == target {
             return true
         } else if array[mid] > target {
@@ -27,13 +27,12 @@ func binarySearch(array: [Int], target: Int) -> Bool {
             min = mid + 1
         }
     }
-    
+
     return false
 }
 
-let sortedGivenArray = givenArray.sorted()
-
-for i in checkedArray.indices {
-    print(binarySearch(array: sortedGivenArray, target: checkedArray[i]) ? 1 : 0)
+for i in compareCards.indices {
+    print(binarySearch(array: havingCards, target: compareCards[i]) ? 1 : 0, terminator: " ")
 }
 
+print()
