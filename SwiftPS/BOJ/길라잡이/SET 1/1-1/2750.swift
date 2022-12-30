f14faed99a45ab655c4ef8f96687069b1f181bb5
YunJoon <kpl88@naver.com>
2022-12-30 13:56:44 +0900

    [BOJ] 2750 수 정렬하기

---
+++ SwiftPS/BOJ/길라잡이/SET 1/1-1/2750.swift
@@ -0,0 +1,33 @@
+//
+//  2750.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2022/12/30.
+//
+
+//import Foundation
+//
+//let N = Int(readLine()!)!
+//var arr: [Int] = []
+//
+//for _ in 0..<N {
+//    let num = Int(readLine()!)!
+//    arr.append(num)
+//}
+//
+//func quickSort(_ arr: [Int]) -> [Int] {
+//    guard arr.count > 1 else { return arr }
+//    
+//    let pivot = arr[0]
+//    let leftArray = arr.filter { $0 < pivot }
+//    let rightArray = arr.filter { $0 > pivot }
+//    return quickSort(leftArray) + [pivot] + quickSort(rightArray)
+//}
+//
+//let sortedArr = quickSort(arr)
+//
+//for i in 0..<sortedArr.count {
+//    print(sortedArr[i])
+//}
+
+/// 링크: https://www.acmicpc.net/problem/2750
--- SwiftPS/main.swift
+++ SwiftPS/main.swift
@@ -1,79 +1,31 @@
-////
-////  main.swift
-////  SwiftPS
-////
-////  Created by Chicken on 2022/05/15.
-////
 //
+//  main.swift
+//  SwiftPS
+//
+//  Created by Chicken on 2022/05/15.
+//
+
 import Foundation
 
-struct Queue {
-    private var Queue: [Int?] = []
-    var front: Int = -1
-    var rear: Int = -1
-
-    mutating func push(_ X: Int) {
-        self.front += 1
-        Queue.append(X)
-    }
-
-    mutating func pop() {
-        if Queue.isEmpty { print("-1") }
-        else {
-            self.rear += 1
-            print(Queue[rear]!)
-            Queue[rear] = nil
-        }
-    }
-
-    func size() {
-        print(Queue.count)
-    }
-
-    func empty() {
-        if Queue.isEmpty { print("1") }
-        else { print("0") }
-    }
-
-    func printFront() {
-        if Queue.isEmpty { print("-1") }
-        else { print(Queue.first!!) }
-    }
-
-    func printBack() {
-        if Queue.isEmpty { print("-1") }
-        else { print(Queue.last!!) }
-    }
-}
-
 let N = Int(readLine()!)!
-var queue = Queue()
+var arr: [Int] = []
 
 for _ in 0..<N {
-    let input = readLine()!.split(separator: " ").map() { String($0) }
-    let command: String = input[0]
+    let num = Int(readLine()!)!
+    arr.append(num)
+}
 
-    switch command {
-    case "push":
-        let num: Int = Int(input[1])!
-        queue.push(num)
+func quickSort(_ arr: [Int]) -> [Int] {
+    guard arr.count > 1 else { return arr }
+    
+    let pivot = arr[0]
+    let leftArray = arr.filter { $0 < pivot }
+    let rightArray = arr.filter { $0 > pivot }
+    return quickSort(leftArray) + [pivot] + quickSort(rightArray)
+}
 
-    case "pop":
-        queue.pop()
+let sortedArr = quickSort(arr)
 
-    case "size":
-        queue.size()
-
-    case "empty":
-        queue.empty()
-
-    case "front":
-        queue.printFront()
-
-    case "back":
-        queue.printBack()
-
-    default:
-        break
-    }
+for i in 0..<sortedArr.count {
+    print(sortedArr[i])
 }
