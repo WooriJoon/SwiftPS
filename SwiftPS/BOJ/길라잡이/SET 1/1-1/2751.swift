53fefe84c31720e48df102c16304d753071fb2be
YunJoon <kpl88@naver.com>
2022-12-30 15:51:18 +0900

    [BOJ] 2751 수 정렬하기 2

---
+++ SwiftPS/BOJ/길라잡이/SET 1/1-1/2751.swift
@@ -0,0 +1,26 @@
+//
+//  2751.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2022/12/30.
+//
+
+/// 2751 수 정렬하기 2
+
+//import Foundation
+//
+//let N = Int(readLine()!)!
+//var arr: [Int] = []
+//
+//for _ in 0..<N {
+//    let num = Int(readLine()!)!
+//    arr.append(num)
+//}
+//
+//let sortedArr = arr.sorted()
+//
+//for i in sortedArr.indices {
+//    print(sortedArr[i])
+//}
+
+/// 링크: https://www.acmicpc.net/problem/2751
