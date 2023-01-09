68a4e964146051e3c5f0cbbcc3d67abec31d98f8
YunJoon <kpl88@naver.com>
2023-01-10 03:04:06 +0900

    [BOJ] 10867 중복 빼고 정렬하기

--- SwiftPS/BOJ/길라잡이/SET 1/1-4/10867.swift
+++ SwiftPS/BOJ/길라잡이/SET 1/1-4/10867.swift
@@ -1,14 +1,16 @@
 //
-//  main.swift
+//  10867.swift
 //  SwiftPS
 //
-//  Created by WooriJoon on 2022/05/15.
+//  Created by WooriJoon on 2023/01/10.
 //
 
-import Foundation
+/// 10867 중복 빼고 정렬하기
 
 let N = Int(readLine()!)!
 var input = Set(readLine()!.split(separator: " ").map() { Int($0)! })
 let answer = input.sorted()
 
 print(answer.map(){String($0)}.joined(separator: " "))
+
+/// 링크: https://www.acmicpc.net/problem/10867
