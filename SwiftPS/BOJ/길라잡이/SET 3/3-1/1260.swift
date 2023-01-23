2216b9642648b78cdcac5f8665575cc84dcea2f8
YunJoon <kpl88@naver.com>
2023-01-23 16:47:37 +0900

    [BOJ] 1260 DFS와 BFS

--- SwiftPS/BOJ/길라잡이/SET 3/3-1/1260.swift
+++ SwiftPS/BOJ/길라잡이/SET 3/3-1/1260.swift
@@ -1,12 +1,11 @@
 //
-//  main.swift
+//  1260.swift
 //  SwiftPS
 //
-//  Created by WooriJoon on 2022/05/15.
+//  Created by WooriJoon on 2023/01/22.
 //
 
-import Foundation
-
+/// 1260 DFS와 BFS
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input[0]
 let M = input[1]
@@ -21,7 +20,7 @@ for _ in 0..<M {
     matrix[node[1]][node[0]] = true
 }
 
-// Queue와 Stack을 이용해 removeLast()로 풀려 했으나 노드의 번호가 작은 것을 먼저 돌아야해서 recursive하게 풀이함
+// Queue와 Stack을 이용해 removeLast()로 풀려 했으나 노드의 번호가 작은 것부터 탐색해야해서 recursive하게 풀이함
 func DFS(_ currentNode: Int) {
     visited[currentNode] = true
     print(currentNode, terminator: " ")
@@ -55,3 +54,5 @@ DFS(V)
 print()
 BFS(V)
 print()
+
+/// https://www.acmicpc.net/problem/1260
