bba5b918004f389ccc577a18e33c85a5bf89e003
YunJoon <kpl88@naver.com>
2023-01-29 15:25:14 +0900

    [BOJ] 1003 피보나치 함수

--- SwiftPS/BOJ/길라잡이/SET 3/3-2/1003.swift
+++ SwiftPS/BOJ/길라잡이/SET 3/3-2/1003.swift
@@ -1,11 +1,11 @@
 //
-//  main.swift
+//  1003.swift
 //  SwiftPS
 //
-//  Created by WooriJoon on 2022/05/15.
+//  Created by WooriJoon on 2023/01/27.
 //
 
-import Foundation
+/// 1003 피보나치 함수
 
 let T = Int(readLine()!)!
 var testCase: [Int] = []
@@ -16,3 +16,5 @@ var answer: [(x: Int, y: Int)] = [(1, 0), (0, 1)]
 for i in 2..<41 { answer.append(((answer[i-1].x + answer[i-2].x), (answer[i-1].y + answer[i-2].y))) }
 
 for i in testCase { print(answer[i].x, answer[i].y) }
+
+/// 링크: https://www.acmicpc.net/problem/1003
