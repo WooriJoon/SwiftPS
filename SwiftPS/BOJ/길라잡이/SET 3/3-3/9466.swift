dd3521ac06b79f2d3a292f9625c240f6d7eb5d1b
YunJoon <kpl88@naver.com>
2023-02-06 02:07:23 +0900

    [BOJ] 9466 텀 프로젝트 주석 추가

--- SwiftPS/BOJ/길라잡이/SET 3/3-3/9466.swift
+++ SwiftPS/BOJ/길라잡이/SET 3/3-3/9466.swift
@@ -9,11 +9,11 @@
 
 let testCase = Int(readLine()!)!
 
-var visited: [Int] = []
-var finished: [Bool] = []
-var selected: [Int] = []
-var order: Int = 0
-var cycle: Int = 0
+var visited: [Int] = []   // 방문한 노드에 방문 순서를 저장
+var finished: [Bool] = [] // N번째 노드가 호출한 함수가 종료되었는지를 저장
+var selected: [Int] = []  // 프로젝트를 함께하고 싶은 학생 선택 결과
+var order: Int = 0        // 노드 방문 순서
+var cycle: Int = 0        // 사이클을 이루고 있는 학생의 수
 
 func DFS(_ node: Int) {
     order += 1
@@ -21,7 +21,8 @@ func DFS(_ node: Int) {
     let next = selected[node-1]
     
     if visited[next] == 0 { DFS(next) }
-    // 이미 간 곳인데 또 갈라고 하면 cycle 발생
+    // 이미 방문한 곳인데 또 방문하면 cycle 발생
+    // 사이클을 이루는 노드의수: 현재 노드의 방문 순서 - 다음 방문할 노드의 방문 순서(사이클을 이루는 첫번째 노드) + 1
     if !finished[next] { cycle += visited[node] - visited[next] + 1 }
     
     finished[node] = true
@@ -29,9 +30,9 @@ func DFS(_ node: Int) {
 
 for _ in 0..<testCase {
     let n = Int(readLine()!)!
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    selected = input
+    selected = readLine()!.split(separator: " ").map { Int($0)! }
     
+    // 인덱싱 편의를 위해 selected.count+1의 크기만큼 배열 지정
     for _ in 0...selected.count { visited.append(0) }
     for _ in 0...selected.count { finished.append(false) }
     
