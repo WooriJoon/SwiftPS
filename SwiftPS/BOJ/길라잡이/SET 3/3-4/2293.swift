d1f009dea908b38932e7fac8a6c9057f1b33b924
YunJoon <kpl88@naver.com>
2023-02-16 16:54:55 +0900

    [BOJ] 2293 동전 1 주석 추가

--- SwiftPS/BOJ/길라잡이/SET 3/3-4/2293.swift
+++ SwiftPS/BOJ/길라잡이/SET 3/3-4/2293.swift
@@ -14,10 +14,12 @@ var coins: [Int] = []
 var DP: [Int] = Array(repeating: 0, count: k+1)
 for _ in 0..<n { coins.append(Int(readLine()!)!) }
 
+// 첫번째 DP는 1 고정 -> i원을 사용하여 i원을 만드는 경우의 수는 1
 DP[0] = 1
 coins.sort()
 
 for i in coins {
+    // stride는 시작할때 인덱스 범위가 벗어나도 에러가 나지 않고 pass함
     for j in stride(from: i, to: k+1, by: 1) {
         DP[j] += DP[j-i] < 2147483648 ? DP[j-i] : 0
     }
