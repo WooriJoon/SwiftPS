b40a33a5c9f096206febdaad3a68b6904b344fc0
YunJoon <kpl88@naver.com>
2023-02-24 16:46:25 +0900

    [BOJ] 11279 최대 힙

---
+++ SwiftPS/BOJ/길라잡이/SET 4/4-3/11279.swift
@@ -0,0 +1,27 @@
+//
+//  11279.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2023/02/24.
+//
+
+/// 11279 최대 힙
+
+//import sys
+//import heapq
+//
+//N = int(sys.stdin.readline())
+//heap = []
+//
+//for _ in range(0, N):
+//    num = int(sys.stdin.readline())
+//
+//    if num == 0:
+//        if len(heap) == 0:
+//            print(0)
+//        else:
+//            print(-1 * heapq.heappop(heap))
+//    else:
+//        heapq.heappush(heap, -1 * num)
+
+/// 링크: https://www.acmicpc.net/problem/11279
