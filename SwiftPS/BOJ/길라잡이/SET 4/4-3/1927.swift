f35b04678303664be070f38ec1f06051d0a1c116
YunJoon <kpl88@naver.com>
2023-02-24 16:46:13 +0900

    [BOJ] 1927 최소 힙

---
+++ SwiftPS/BOJ/길라잡이/SET 4/4-3/1927.swift
@@ -0,0 +1,29 @@
+//
+//  1927.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2023/02/24.
+//
+
+/// 1927 최소 힙
+
+//import sys
+//import heapq
+//
+//N = int(sys.stdin.readline())
+//heap = []
+//
+//for _ in range(0, N):
+//    num = int(sys.stdin.readline())
+//
+//    if num == 0:
+//        if len(heap) == 0:
+//            print(0)
+//        else:
+//            print(heapq.heappop(heap))
+//    else:
+//        heapq.heappush(heap, num)
+//
+//    print(heap)
+
+/// 링크: https://www.acmicpc.net/problem/1927
