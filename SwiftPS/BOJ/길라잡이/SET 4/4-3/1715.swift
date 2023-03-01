e165e31e451244fab9c49e5e6dd0c27329d0efb5
YunJoon <kpl88@naver.com>
2023-03-01 20:41:16 +0900

    [BOJ] 1715 카드 정렬하기

---
+++ SwiftPS/BOJ/길라잡이/SET 4/4-3/1715.swift
@@ -0,0 +1,29 @@
+//
+//  1715.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2023/02/24.
+//
+
+/// 1715 카드 정렬하기
+
+//import sys
+//import heapq
+//
+//N = int(sys.stdin.readline())
+//cards = []
+//sum = 0
+//count = 0
+//
+//for _ in range(0, N):
+//    num = int(sys.stdin.readline())
+//    heapq.heappush(cards, num)
+//
+//while len(cards) > 1:
+//    sum = heapq.heappop(cards) + heapq.heappop(cards)
+//    count += sum
+//    heapq.heappush(cards, sum)
+//
+//print(count)
+
+/// 링크: https://www.acmicpc.net/problem/1715
