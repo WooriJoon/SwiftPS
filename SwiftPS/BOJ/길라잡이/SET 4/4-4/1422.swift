ec6b59f5102e87cd682e65fa7cd1786cbdb71397
YunJoon <kpl88@naver.com>
2023-03-07 17:39:26 +0900

    [BOJ] 1422 숫자의 신

--- SwiftPS/BOJ/길라잡이/SET 4/4-4/1422.swift
+++ SwiftPS/BOJ/길라잡이/SET 4/4-4/1422.swift
@@ -1,11 +1,11 @@
 //
-//  main.swift
+//  1422.swift
 //  SwiftPS
 //
-//  Created by WooriJoon on 2022/05/15.
+//  Created by WooriJoon on 2023/03/02.
 //
 
-import Foundation
+/// 1422 숫자의 신
 
 let KN = readLine()!.split(separator: " ").map { Int($0)! }
 let K = KN[0]
@@ -13,13 +13,15 @@ let N = KN[1]
 var nums: [String] = []
 for _ in 0..<K { nums.append(readLine()!) }
 
+// 어떤 원소를 앞에 둘지 정렬
 nums.sort { ($0 + $1) > ($1 + $0) }
 
-let max = nums.map { Int($0)! }.max()!
-var isMaxUsed = false
+let max = nums.map { Int($0)! }.max()! // 가장 큰 값을 최대한 많이 넣어야하기 때문에 따로 뺌
+var isMaxUsed = false                  // max 값이 쓰였는지 체크
 var answer: [String] = []
 
 for i in 0..<K {
+    // 가장 큰 값을 N-K+1(넣을 수 있는 최대)번 넣음
     if !isMaxUsed && Int(nums[i])! == max {
         isMaxUsed = true
         for _ in 0..<N-K+1 { answer.append(nums[i]) }
@@ -28,3 +30,5 @@ for i in 0..<K {
 }
 
 print(answer.joined())
+
+/// 링크: https://www.acmicpc.net/problem/1422
---
+++ SwiftPS/BOJ/길라잡이/SET 4/4-4/9574.swift
@@ -0,0 +1,8 @@
+//
+//  9574.swift
+//  SwiftPS
+//
+//  Created by WooriJoon on 2023/03/04.
+//
+
+import Foundation
